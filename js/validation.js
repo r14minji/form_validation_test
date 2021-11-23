9f012b75d2e9475ad44e1c5cd1f41d21f79b605d
r14minji <rkdalswl0728@gmail.com>
2021-11-23 12:46:17 +0900

    step7 class 객체지향

--- js/validation.js
+++ js/validation.js
@@ -1,129 +1,134 @@
-var form = document.querySelector("#member");
-var btnSubmit = document.querySelector("input[type=submit]");
+class Validation{
 
-btnSubmit.addEventListener("click", function(e){
-  if(!isTxt("userid", 5)) e.preventDefault();
-  if(!isTxt("comments", 20)) e.preventDefault();
-  if(!isEmail("email", 5)) e.preventDefault();
-  if(!isCheck("gender")) e.preventDefault();
-  if(!isCheck("hobby")) e.preventDefault();
-  if(!isSelect("edu")) e.preventDefault();
-  if(!isPwd("pwd1", "pwd2", 5)) e.preventDefault();
-})
-
-function isTxt(name, len){
-  var input = form.querySelector(`[name=${name}]`);
-  var txt = input.value;
+  constructor(){
+    this.form = document.querySelector("#member");
+    this.btnSubmit = document.querySelector("input[type=submit]");
   
-  if( txt.length >= len && txt != ""){
-    const errMsgs = document.closest("td").querySelectorAll("p");
-    if(errMsgs.length > 0 )  input.closest("td").querySelector("p").remove();
-
-    return true;
-  }else{
-    var errMsgs = input.closest("td").querySelectorAll("p");
-    if(errMsgs.length > 0 )  input.closest("td").querySelector("p").remove();
-
-    var errMsg = document.createElement("p");
-    errMsg.append(`입력항목을 ${len}글자 이상 입력하세요.`);
-    input.closest("td").append(errMsg); 
-    return false;
-  }
-}
-
-function isEmail(name, len){
-  var input = form.querySelector(`[name=${name}]`);
-  var txt = input.value;
-  //txt.includes("@")
-  if(txt.length> len && /@/.test(txt)){
-    var errMsgs = input.closest("td").querySelectorAll("p");
-    if(errMsgs.length > 0 )  input.closest("td").querySelector("p").remove();
-    return true;
-  }else{
-    var errMsgs = input.closest("td").querySelectorAll("p");
-    if(errMsgs.length > 0 )  input.closest("td").querySelector("p").remove();
-
-    var errMsg = document.createElement("p");
-    errMsg.append(`@를 포함한 이메일 주소를 ${len}글자 이상 입력하세요.`);
-    input.closest("td").append(errMsg); 
-    return false;
-  }
-}
-
-function isCheck(name){
-  var inputs = form.querySelectorAll(`[name=${name}]`);
-  var isChecked = false;
-
-  for(var input of inputs){
-    if(input.checked) isChecked = true;
+    this.btnSubmit.addEventListener("click", e=>{
+      if(!this.isTxt("userid", 5)) e.preventDefault();
+      if(!this.isTxt("comments", 20)) e.preventDefault();
+      if(!this.isEmail("email", 5)) e.preventDefault();
+      if(!this.isCheck("gender")) e.preventDefault();
+      if(!this.isCheck("hobby")) e.preventDefault();
+      if(!this.isSelect("edu")) e.preventDefault();
+      if(!this.isPwd("pwd1", "pwd2", 5)) e.preventDefault();
+    })
   }
 
-  if(isChecked){
-    var errMsgs = inputs[0].closest("td").querySelectorAll("p");
-    if(errMsgs.length > 0) input[0].closest("td").querySelector("p").remove();
-    return true;
-  }else{
-    var errMsgs = inputs[0].closest("td").querySelectorAll("p"); 
-    if(errMsgs.length > 0) inputs[0].closest("td").querySelector("p").remove();
-
-    var errMsg = document.createElement("p");
-    errMsg.append("필수 입력사항을 체크하세요.");
-    inputs[0].closest("td").append(errMsg);
-    return false;
-  }
-}
-
-function isSelect(name){
-  var sel= form.querySelector(`[name = ${name}]`);
-  
-  //var sel_index = sel.options.selectedIndex;
-  //var val = sel.options[sel_index].value;
-  var val = sel.value;
-
-  if(val !== ""){
-    var errMsgs = sel.closest("td").querySelectorAll("p");
-    if(errMsgs.length > 0) sel.closest("td").querySelector("p").remove();
-
-    return true;
-  }else{
-    var errMsgs = sel.closest("td").querySelectorAll("p");
-    if(errMsgs.length > 0) sel.closest("td").querySelector("p").remove();
-
-    var errMsg = document.createElement("p");
-    errMsg.append("항목을 선택해주세요");
-    sel.closest("td").append(errMsg);
+  isTxt(name, len){
+    const input = this.form.querySelector(`[name=${name}]`);
+    const txt = input.value;
     
-    return false;
+    if( txt.length >= len && txt != ""){
+      const errMsgs = document.closest("td").querySelectorAll("p");
+      if(errMsgs.length > 0 )  input.closest("td").querySelector("p").remove();
+
+      return true;
+    }else{
+      const errMsgs = input.closest("td").querySelectorAll("p");
+      if(errMsgs.length > 0 )  input.closest("td").querySelector("p").remove();
+
+      const errMsg = document.createElement("p");
+      errMsg.append(`입력항목을 ${len}글자 이상 입력하세요.`);
+      input.closest("td").append(errMsg); 
+      return false;
+    }
+  }
+
+  isEmail(name, len){
+    const input = this.form.querySelector(`[name=${name}]`);
+    const txt = input.value;
+    //txt.includes("@")
+    if(txt.length> len && /@/.test(txt)){
+      const errMsgs = input.closest("td").querySelectorAll("p");
+      if(errMsgs.length > 0 )  input.closest("td").querySelector("p").remove();
+      return true;
+    }else{
+      const errMsgs = input.closest("td").querySelectorAll("p");
+      if(errMsgs.length > 0 )  input.closest("td").querySelector("p").remove();
+
+      const errMsg = document.createElement("p");
+      errMsg.append(`@를 포함한 이메일 주소를 ${len}글자 이상 입력하세요.`);
+      input.closest("td").append(errMsg); 
+      return false;
+    }
+  }
+
+  isCheck(name){
+    const inputs = this.form.querySelectorAll(`[name=${name}]`);
+    let isChecked = false;
+
+    for(let input of inputs){
+      if(input.checked) isChecked = true;
+    }
+
+    if(isChecked){
+      const errMsgs = inputs[0].closest("td").querySelectorAll("p");
+      if(errMsgs.length > 0) input[0].closest("td").querySelector("p").remove();
+      return true;
+    }else{
+      const errMsgs = inputs[0].closest("td").querySelectorAll("p"); 
+      if(errMsgs.length > 0) inputs[0].closest("td").querySelector("p").remove();
+
+      const errMsg = document.createElement("p");
+      errMsg.append("필수 입력사항을 체크하세요.");
+      inputs[0].closest("td").append(errMsg);
+      return false;
+    }
+  }
+
+  isSelect(name){
+    const sel= this.form.querySelector(`[name = ${name}]`);
+    
+    //var sel_index = sel.options.selectedIndex;
+    //var val = sel.options[sel_index].value;
+    const val = sel.value;
+
+    if(val !== ""){
+      const errMsgs = sel.closest("td").querySelectorAll("p");
+      if(errMsgs.length > 0) sel.closest("td").querySelector("p").remove();
+
+      return true;
+    }else{
+      const errMsgs = sel.closest("td").querySelectorAll("p");
+      if(errMsgs.length > 0) sel.closest("td").querySelector("p").remove();
+
+      const errMsg = document.createElement("p");
+      errMsg.append("항목을 선택해주세요");
+      sel.closest("td").append(errMsg);
+      
+      return false;
+    }
+  }
+
+  isPwd(name1, name2, len){
+    const pwd1 = this.form.querySelector(`[name=${name1}]`);
+    const pwd2 = this.form.querySelector(`[name=${name2}]`);
+    const pwd1_val = pwd1.value;
+    const pwd2_val = pwd2.value;
+
+
+    const eng = /[a-zA-Z]/;
+    const num = /[0-9]/;
+    const spc = /[~!@#$%^&*()_+-=]/;
+    
+
+    if(pwd1_val === pwd2_val && pwd1.length> len && eng.test(pwd1_val) && num.test(pwd1_val) && spc.test(pwd1_val)){
+      const errMsgs = pwd1.closest("td").querySelectorAll("p");
+      if(errMsgs.length>0) pwd1.closest("td").querySelector("p").remove();
+
+      return true;
+    }else{
+
+      const errMsgs = pwd1.closest("td").querySelectorAll("p");
+      if(errMsgs.length>0) pwd1.closest("td").querySelector("p").remove();
+
+      const errMsg = document.createElement("p");
+      errMsg.append(`비밀번호를 ${len}글자 이상, 영문, 숫자, 특수문자 모두 포함해서 동일하게 입력하세요`);
+      pwd1.closest("td").append(errMsg);
+
+      return false;
+    }
   }
 }
 
-function isPwd(name1, name2, len){
-  var pwd1 = form.querySelector(`[name=${name1}]`);
-  var pwd2 = form.querySelector(`[name=${name2}]`);
-  var pwd1_val = pwd1.value;
-  var pwd2_val = pwd2.value;
-
-
-  var eng = /[a-zA-Z]/;
-  var num = /[0-9]/;
-  var spc = /[~!@#$%^&*()_+-=]/;
-
-
-  if(pwd1_val === pwd2_val && pwd1.length> len && eng.test(pwd1_val) && num.test(pwd1_val) && spc.test(pwd1_val)){
-    var errMsgs = pwd1.closest("td").querySelectorAll("p");
-    if(errMsgs.length>0) pwd1.closest("td").querySelector("p").remove();
-
-    return true;
-  }else{
-
-    var errMsgs = pwd1.closest("td").querySelectorAll("p");
-    if(errMsgs.length>0) pwd1.closest("td").querySelector("p").remove();
-
-    var errMsg = document.createElement("p");
-    errMsg.append(`비밀번호를 ${len}글자 이상, 영문, 숫자, 특수문자 모두 포함해서 동일하게 입력하세요`);
-    pwd1.closest("td").append(errMsg);
-
-    return false;
-  }
-
-}
